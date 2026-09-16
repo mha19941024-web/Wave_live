fd6a344ef1cf21559f0bd612336faff414c4a848
mha19941024-web <mha19941024@gmail.com>
2026-09-16 03:23:51 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,3 +64,7 @@ dependencies {
 
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
+plugins {
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.21" apply false
+}
