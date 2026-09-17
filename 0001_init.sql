bbce0ea87779be6ada760da0df79d837634ad989
mha19941024-web <mha19941024@gmail.com>
2026-09-17 17:44:47 +0300

    Delete 0001_init.sql

--- 0001_init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS videos (
-  id TEXT PRIMARY KEY,
-  url TEXT NOT NULL,
-  user TEXT NOT NULL,
-  caption TEXT NOT NULL DEFAULT '',
-  likes INTEGER NOT NULL DEFAULT 0,
-  created_at TEXT NOT NULL
-);
-CREATE INDEX IF NOT EXISTS idx_videos_created_at ON videos(created_at DESC);
