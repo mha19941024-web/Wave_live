14d3db092b0e0de000e54a06a2c7c582463cb5d0
mha19941024-web <mha19941024@gmail.com>
2026-09-17 17:46:06 +0300

    Delete 0003_engagement.sql

--- 0003_engagement.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS video_likes (
-  video_id TEXT NOT NULL,
-  user_id TEXT NOT NULL,
-  created_at TEXT NOT NULL,
-  PRIMARY KEY (video_id, user_id)
-);
-CREATE INDEX IF NOT EXISTS idx_video_likes_user ON video_likes(user_id);
